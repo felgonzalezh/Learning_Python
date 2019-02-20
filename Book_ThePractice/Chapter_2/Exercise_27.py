bb8dca4f767bbc6d980fad298bff05d17746f483
felgonzalezh <felgonzalezh@gmail.com>
2019-02-20 16:21:51 -0500

    quadratic equation

---
+++ Book_ThePractice/Chapter_2/Exercise_27.py
@@ -0,0 +1,28 @@
+import math;
+# This programm finds the roots for a quadratic equation:
+
+print "This program finds the roots for a quadratic equation Ax2 + Bx + C = 0";
+a = int(raw_input("Type the parameter A= "));
+b = int(raw_input("Type the parameter B= "));
+c = int(raw_input("Type the parameter C= "));
+
+root = b*b - 4*a*c;
+
+if(root < 0):
+    print "The solution is an imaginary number ";
+    root_i = math.sqrt(abs(root))*1j;
+    x1 = (-b+root_i)/(2*a);
+    x2 = (-b-root_i)/(2*a);
+    print "One root: ", x1;
+    print "Second root: ", x2;
+elif(root == 0):
+    x1 = -b/(2*a);
+    print "There is only ONE root: ", x1;
+else:
+    x1 = (-b+math.sqrt(root))/(2*a);
+    x2 = (-b-math.sqrt(root))/(2*a);
+    print "One root: ", x1;
+    print "Second root: ", x2;
+
+
+    
